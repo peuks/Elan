bd67ae9dec62faa7cbe8b95b387f3027ffeee40a
david vanmak <vanmakdavid.lwg@gmail.com>
2020-12-23 09:20:07 +0100

    Fix case-sensitive from Windows to gnu/linux

---
+++ elan-PHP/appli/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'e892a3a94eda97922e469ab69bee3acf467a40bf',
+    'name' => 'stagiaire/appli',
+  ),
+  'versions' => 
+  array (
+    'stagiaire/appli' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e892a3a94eda97922e469ab69bee3acf467a40bf',
+    ),
+  ),
+);
